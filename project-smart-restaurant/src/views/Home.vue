efc49415e446edb961735211e894493f866afd7d
Hercules <herculesprogramador911@gmail.com>
2023-02-11 11:58:26 -0300

    add new file JSON and Axios for aplication

--- project-smart-restaurant/src/views/Home.vue
+++ project-smart-restaurant/src/views/Home.vue
@@ -3,13 +3,17 @@
 </template>
 <script>
 import categoryMenu from '../components/CategoryMenu.vue'
+import axios from 'axios'
 export default {
     name: 'Home',
     components: {
         categoryMenu
+    },
+    created() {
+        axios.get('http://localhost:3000/burguers').then(function (response) {
+            // manipula o sucesso da requisição
+            console.log(response);
+        })
     }
 }
 </script>
-<style lang="">
-    
-</style>
